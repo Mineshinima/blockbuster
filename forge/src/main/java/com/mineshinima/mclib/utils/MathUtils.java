9939edad22d533d97f3ad0aa6ff8a1b28d531d5a
Chryfi <71967555+Chryfi@users.noreply.github.com>
2023-09-18 00:28:44 +0200

    tweak: refactor MathUtils and fix raw use of parameterized class

--- forge/src/main/java/com/mineshinima/mclib/utils/MathUtils.java
+++ forge/src/main/java/com/mineshinima/mclib/utils/MathUtils.java
@@ -5,27 +5,27 @@ public class MathUtils {
         return value < min ? min : (value > max ? max : value);
     }
 
-    public static<T extends Comparable> T clamp(T value, T min, T max) {
+    public static<T extends Comparable<T>> T clamp(T value, T min, T max) {
         return value.compareTo(min) < 0 ? min : (value.compareTo(max) > 0 ? max : value);
     }
 
     public static float clamp(float value, float min, float max) {
-        return value < min ? min : (value > max ? max : value);
+        return value < min ? min : Math.min(value, max);
     }
 
     public static double clamp(double value, double min, double max) {
-        return value < min ? min : (value > max ? max : value);
+        return value < min ? min : Math.min(value, max);
     }
 
     public static long clamp(long value, long min, long max) {
-        return value < min ? min : (value > max ? max : value);
+        return value < min ? min : Math.min(value, max);
     }
 
-    public static<T extends Comparable> T max(T a, T b) {
+    public static<T extends Comparable<T>> T max(T a, T b) {
         return a.compareTo(b) < 0 ? b : a;
     }
 
-    public static<T extends Comparable> T min(T a, T b) {
+    public static<T extends Comparable<T>> T min(T a, T b) {
         return a.compareTo(b) < 0 ? a : b;
     }
 
